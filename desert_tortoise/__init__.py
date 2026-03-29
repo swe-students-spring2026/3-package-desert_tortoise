bf3344ac1b1a423223d981aeb2f85eff2eda5939
ginny1536 <ginny1536@outlook.com>
2026-03-29 16:14:16 -0400

    added dog to init file

--- desert_tortoise/__init__.py
+++ desert_tortoise/__init__.py
@@ -5,5 +5,6 @@ from .tortoise import Tortoise
 from .parrot import Parrot
 from . import shelter
 from .bunny import Bunny
+from .dog import Dog
 
-__all__ = ["Pet", "Parrot", "Bunny", "shelter", "Tortoise"]
+__all__ = ["Pet", "Parrot", "Bunny", "shelter", "Tortoise", "Dog"]
